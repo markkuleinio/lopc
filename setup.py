ef956aaddfe8186619942db2b2c382f7dabdca4c
Markku Leiniö <markkuleinio@users.noreply.github.com>
2022-11-27 20:57:16 +0200

    Update version to 1.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 
 setup(
     name="lopc",
-    version="1.0.1",
+    version="1.0.2",
     description="Counts lines of Python code",
     long_description=long_description,
     long_description_content_type="text/markdown",
