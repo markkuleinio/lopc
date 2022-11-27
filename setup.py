4412ee8216f34d23a6c00ea84608cbf7ae9518ed
Markku Leiniö <markkuleinio@users.noreply.github.com>
2022-11-27 17:38:15 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+with open("README.md") as f:
+    long_description = f.read()
+
+
+setup(
+    name="lopc",
+    version="1.0.0",
+    description="Counts lines of Python code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Markku Leiniö",
+    python_requires=">=3.7",
+    packages=["lopc"],
+    entry_points={"console_scripts": ["lopc=lopc:main"]},
+    license="MIT",
+)
